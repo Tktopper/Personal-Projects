a07bd9a1c2fc8093934631ce78565c670bc97e68
Tanner Kleintop <taktopper@gmail.com>
2023-09-20 23:19:19 -0400

    Started adding simple projects

---
+++ C++/Hello World/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream> // Imports the iostream library
+
+using namespace std; // Uses the std namespace
+
+int main(){
+    cout << "Hello World!" << endl; // Prints Hello World!
+    return 0; // Returns 0
+}
